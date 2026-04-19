6dd682f20ec15571f6de9b8bc46257a1fd8298d2
markus20_11 <gamepostmarkus@gmail.com>
2026-04-19 13:32:38 +0200

    Refactor server.js for improved structure and functionality
    Added server initialization and pixel drawing functionality.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 
-app.use(express.static('public'));
+app.use(express.static('./'));
 
 let pixels = {}; 
 let userStats = {}; 
